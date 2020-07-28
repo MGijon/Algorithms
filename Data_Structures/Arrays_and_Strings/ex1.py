21e9ba1564aeab45d1e80edad82a648a16c7d0ad
Manuel Gijón <mgijon94@gmail.com>
2020-07-28 20:18:52 +0200

    Data Structures -> Arrays and Strings -> (Python) ex1 done, TODO: second variation (in the book).

--- Data_Structures/Arrays_and_Strings/ex1.py
+++ Data_Structures/Arrays_and_Strings/ex1.py
@@ -4,6 +4,8 @@ IS_UNIQUE:
 Implement an algorithm to determine if a string has all unique characteres.
 What if you cannot use additional data structures?
 """
+import time
+import unittest
 
 class ex1():
     """
@@ -29,12 +31,24 @@ class ex1():
 
     def solution_2(self, input_string):
         """
+        Suppose the codification is ASCII.
         :param self:
         :param input_string:
         """
 
-        return 0
+        if len(input_string) > 128:
+            return False
 
+        char_list = [False for _ in range(128)]
+        for char in input_string:
+            value = ord(char)
+            if char_list[value]:
+                return False
+            char_list[value] = True
+
+        return True
+
+    '''
     def solution_3(self, input_string):
         """
         :param self:
@@ -42,20 +56,10 @@ class ex1():
         """
 
         return 0
-
+    '''
     def test(self):
         """
-        La idea aquí es escribir los tests para esta cosa,
-        uno por solución.
-
-
-        IDEA: escirbir funciones auxiliares como decoradores
-        para printar las cosas....
-        IDEA: añadir una barra de progreso y si es posible también
-        un log con los tiempos para evaluar como funcionan las
-        diferentes funciones.
-
-        TODO: sacar los elementos de tests fuera de la función muhahhahahahhahaha
+        Testing functions. Test every solution and print times and accuracy.
         """
 
         test_elements = {
@@ -67,6 +71,7 @@ class ex1():
         print('Solution 1:')
         counter = 0
         trues = 0
+        start = time.time()
         for sample in test_elements:
             true_value = test_elements[sample]
             predicted_value = self.solution_1(input_string=sample)
@@ -77,15 +82,19 @@ class ex1():
                 trues+=1
             counter+=1
 
+        end = time.time()
+        print()
+
         print('\t- Total tested cases: {}'.format(counter))
         if counter==trues:
             print('\t- Works fine in all tested cases.')
-        print('\t- TIME TOOOOOODDDDDDDOOOOO\n')
+        print('\t- Elapsed time {}\n'.format(end - start))
             #print(sample, ' - True Value: ', true_value, ' - Predicted:', predicted_value)
 
         print('Solution 2:')
         counter = 0
         trues = 0
+        start = time.time()
         for sample in test_elements:
             true_value = test_elements[sample]
             predicted_value = self.solution_2(input_string=sample)
@@ -94,15 +103,19 @@ class ex1():
                 trues += 1
             counter +=1
 
+        end = time.time()
+        print()
+
         print('\t- Total tested cases: {}'.format(counter))
         if counter==trues:
             print('\t- Works fine in all tested cases.')
-        print('\t- TIME TOOOOOODDDDDDDOOOOO\n')
+        print('\t- Elapsed time {}\n'.format(end - start))
 
-
+        '''
         print('Solution 3:')
         counter = 0
         trues = 0
+        start = time.time()
         for sample in test_elements:
             true_value = test_elements[sample]
             predicted_value = self.solution_3(input_string=sample)
@@ -111,11 +124,14 @@ class ex1():
                 trues += 1
             counter +=1
 
+        end = time.time()
+        print()
 
         print('\t- Total tested cases: {}'.format(counter))
         if counter==trues:
             print('\t- Works fine in all tested cases.')
-        print('\t- TIME TOOOOOODDDDDDDOOOOO\n')
+        print('\t- Elapsed time {}\n'.format(end - start))
+        '''
 
 
 
