f33a675f3848779839b1deb47c944b46ecb14033
manuel <mgijon94@gmail.com>
2020-09-10 23:49:14 +0200

    merge sort in python

---
+++ Algorithms/Sorting_and_Searching/mergeSort.py
@@ -0,0 +1,37 @@
+"""TODO"""
+
+def mergeSort(array):
+
+    if len(array) > 1:
+        mid = len(array)//2
+        left = array[:mid]
+        right = array[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i, j, k = 0, 0, 0
+        while (i < len(left)) and (j < len(right)):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+
+if __name__ == '__main__':
+    array = [1, 21, 15, 5, 6, 854, 6]
+    print('The given array is ', array)
+    mergeSort(array)
+    print('The sorted array is ', array)
