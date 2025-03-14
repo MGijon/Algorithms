85d145de5b4d3b012b7ede1e9e453b1948ea15f4
Manuel Gijón Agudo <mgijon94@gmail.com>
2025-03-14 20:35:22 +0100

    wip solving problem
    - Solution correct but not in time constrains.

--- Solved Exercises/codeForce/7_divisibility_problem/solution.cpp
+++ Solved Exercises/codeForce/7_divisibility_problem/solution.cpp
@@ -1,68 +1,56 @@
-/*
-time limit per test1 second
-memory limit per test256 megabytes
-You are given two positive integers 𝑎
- and 𝑏
-. In one move you can increase 𝑎
- by 1
- (replace 𝑎
- with 𝑎+1
-). Your task is to find the minimum number of moves you need to do in order to make 𝑎
- divisible by 𝑏
-. It is possible, that you have to make 0
- moves, as 𝑎
- is already divisible by 𝑏
-. You have to answer 𝑡
- independent test cases.
-
-Input
-The first line of the input contains one integer 𝑡
- (1≤𝑡≤104
-) — the number of test cases. Then 𝑡
- test cases follow.
-
-The only line of the test case contains two integers 𝑎
- and 𝑏
- (1≤𝑎,𝑏≤109
-).
-
-Output
-For each test case print the answer — the minimum number of moves you need to do in order to make 𝑎
- divisible by 𝑏.
-*/
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int computeNumberOfSteps(string line) {
-	vector<string> v;
-  cout << line << endl;
-  // ---------
-  string delimiter = " ";
-
-  size_t found = str.find(delimiter);
-  while (found != string::npos)
+/**
+ * Split a string into a vector of integers.
+ * @param line: string to split
+ * @return vector<int>: vector of integers
+ */
+vector<int> splitString(string line)
+{
+  vector<int> v;
+  string temp = "";
+  for (int i = 0; i < line.size(); i++)
   {
-    string token = str.substr(0, found);
-    str = str.substr(found + delimiter.length(), str.length() - 1);
-    cout << token << '\n';
-    found = str.find(delimiter);
+    if (line[i] == ' ')
+    {
+      v.push_back(stoi(temp)); // push_back: add element to the end of the vector
+      temp = "";
+    }
+    else
+    {
+      temp += line[i];
+    }
   }
-  // ---------
-    return 0;
+  v.push_back(stoi(temp)); // stoi: string to integer
+  return v;
 };
 
-int main(){
+int main()
+{
   string line;
   int lineCounter = 0;
-  while (getline(cin, line)){
-    if (lineCounter != 0){
-      int number = computeNumberOfSteps(line);
+  while (getline(cin, line))
+  {
+    if (lineCounter != 0)
+    {
+      vector<int> numbers = splitString(line);
+      int base = numbers[0];
+      int divisor = numbers[1];
+
+      int number_of_steps = 0;
+
+      while (base % divisor != 0)
+      {
+        base += 1;
+        number_of_steps += 1;
+      }
+      cout << number_of_steps << endl;
     }
     lineCounter += 1;
   }
   return 0;
 }
-
-
