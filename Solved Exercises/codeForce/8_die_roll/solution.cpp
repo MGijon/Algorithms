4ed28a9f0840623d71293c83c5f1c8e7f996c751
Manuel Gijón <manuelgijon@MacBook-Pro-de-Manuel-3.local>
2025-03-21 15:39:56 +0100

    working on solving a problem
    - From CodeForces, still on it

--- Solved Exercises/codeForce/8_die_roll/solution.cpp
+++ Solved Exercises/codeForce/8_die_roll/solution.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+vector<int> returnTwoNumbers(string line){
+  vector<int> twoNumbers;
+
+  twoNumbers.push_back(line[0] - '0');  // we can do it like these only because in this case numbers < 10, one digit
+  twoNumbers.push_back(line[2] - '0');
+  return twoNumbers;
+} 
+
 int main()
 {
+  string line; 
+  int numerator;
+  int denominator;
+
+  while (getline(cin, line)){
+    numerator = returnTwoNumbers(line)[1];
+    denominator = returnTwoNumbers(line)[0];
+  }
+  if (
+      (numerator % 2) && (denominator % 2) 
+     ){
+    numerator = numerator / 2;
+    denominator = denominator / 2;
+  }
+  string output = buildIrreducibleFraction(numerator, denominator);
+  cout << output << endl;
 
   return 0;
 }
